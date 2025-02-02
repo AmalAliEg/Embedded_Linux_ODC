2013ae79359a86d947c5bd4a08b8d0e03baded3f
Amal_Aly <aml_ali_hassan@outlook.com>
2025-02-03 01:33:46 +0200

    fINAL DRAFT

--- Week1/MiniProject/bank_sys.sh
+++ Week1/MiniProject/bank_sys.sh
@@ -80,6 +80,7 @@ Save_Database(){
     fi 
 
     #count the creation number of the backup files
+
     count=$(ls "$BackUp_DIR" | grep -c "^backup_" )
 
     #check over the condition and delete the oldest five files to save the memory
@@ -97,6 +98,75 @@ Save_Database(){
 }
 
 
+update_client(){
+    read -p "enter id of the client to update:"  Client_ID
+
+    if grep -q "^$Client_ID" "$DATABASE";  then
+        read -p "enter new name of the client to update:" Client_Name
+        read -p "enter new account number of the client to update:" Client_Account_NO
+        read -p "enter new phone number of the client to update:" Client_Phone_NO
+        read -p "enter new amount of the account to update:" Fund_Ammount
+        sed -i "/^$Client_ID/d" "$DATABASE"
+        printf "%-20s | %-15s | %-15s | % -15s| %s\n""$Client_ID" "$Client_Name" "$Cient_Account_NO""$Cient_Phone_NO""$nd_Ammount" >> "$DATABASE"
+        echo "client updated successfully"
+    else 
+        echo "Client with Id  : $Client_ID  does not exist "
+        
+    fi 
+     
+      
+
+}
+Search_Client(){
+    read -p "enter id of the client to search :" Client_ID
+    if grep -q "^$Client_ID" "$DATABASE";  then
+        grep "^$Client_ID" "$DATABASE"
+    else
+        echo "Client with ID : $Client_ID does not exist "
+    fi
+
+
+}
+List_Clients() {
+    if [ -s "$DATABASE" ]; then
+        echo "listing all clients:"
+        echo  "------------------------------"
+        cat "$DATABASE"
+        echo "---------------------------"
+    else
+        echo "no clients found "
+    fi
+    
+}
+
+Main_menu (){
+    while true; do
+       echo "--------------------------------------"
+       echo "Main menu "
+       echo "1. Add Client"
+       echo "2. Delete Client"
+       echo "3. Update Client"
+       echo "4. Search Client"
+       echo "5. List Clients"
+       echo "6.Save Database Backup"
+       echo "7. Exit"
+       echo "--------------------------------------------------"
+       read -p "Choose an option: " choice
+
+       case $choice in
+           1) Add_clients ;;
+           2) Delete_Client ;;
+           3) update_client ;;
+           4) Search_Client ;;
+           5) List_Clients ;;
+           6) Save_Database ;;
+           7) echo " exitin..." ; break ;;
+           *) echo "invalid option. please try again "
+        esac
+    done
+}
+init_Database
+Main_menu
 #Call the init function
 #init_Database          
 
