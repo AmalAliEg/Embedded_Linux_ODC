4eb5b7fc8d26cdd5b42f93469446e75662f58b7f
Amal_Aly <aml_ali_hassan@outlook.com>
2025-02-23 10:15:35 +0200

    deleted:    Grad_Project/Project_Description.md 	deleted:    Grad_Project/smart_project.spec 	renamed:    Grad_Project/.gitignore -> Week4_Grad_Project/.gitignore 	renamed:    Grad_Project/CarScreen.py -> Week4_Grad_Project/CarScreen.py 	renamed:    Grad_Project/HomeScreen.py -> Week4_Grad_Project/HomeScreen.py 	renamed:    Grad_Project/MainScreen.py -> Week4_Grad_Project/MainScreen.py 	new file:   Week4_Grad_Project/Project_Description.md 	renamed:    Grad_Project/RoomsScreen.py -> Week4_Grad_Project/RoomsScreen.py 	renamed:    Grad_Project/TvScreen.py -> Week4_Grad_Project/TvScreen.py 	renamed:    Grad_Project/parking.mp4 -> Week4_Grad_Project/parking.mp4 	new file:   Week4_Grad_Project/presentation/prezi.pptx 	renamed:    Grad_Project/requirments.txt -> Week4_Grad_Project/requirments.txt 	renamed:    Grad_Project/setup.py -> Week4_Grad_Project/setup.py 	renamed:    weekk4/hello.cpp -> Week4_Grad_Project/task/hello.cpp 	renamed:    Grad_Project/utils.py -> Week4_Grad_Project/utils.py

---
+++ Week4_Grad_Project/task/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main ()
+{
+
+    std::cout<< "Hello from the GitHub, come up there!" <<std::endl;
+
+    return 0;
+}
--- weekk4/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main ()
-{
-
-    std::cout<< "Hello from the GitHub, come up there!" <<std::endl;
-
-    return 0;
-}
